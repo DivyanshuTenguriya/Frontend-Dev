a608b1a41a0d4f29da86ab5d9dc78583b7bfc458
DivyanshuTenguriya <divyanshutenguriya@gmail.com>
2025-11-20 10:28:46 +0530

    [Divyanshu] add: Callback_Function

--- JS Function/Callback_Function.js
+++ JS Function/Callback_Function.js
@@ -1,11 +1,9 @@
 function greetUser(name, callback) {
     console.log("Hello " + name);
-    callback();  // calling the callback
-}
+    callback(); 
 
 function showEndMessage() {
     console.log("Welcome to the course!");
 }
 
-// calling main function
 greetUser("Divyanshu", showEndMessage);
