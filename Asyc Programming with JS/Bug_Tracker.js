e432e8881ae0f174dc468a5fe87fbe00ece0d8a9
DivyanshuTenguriya <divyanshutenguriya@gmail.com>
2025-11-23 20:09:05 +0530

    [Divyanshu] add: Bug_Tracker

---
+++ Asyc Programming with JS/Bug_Tracker.js
@@ -0,0 +1,12 @@
+function getBugs() {
+    return new Promise((resolve, reject) => {
+        const failed = Math.random() < 0.3;
+        setTimeout(() => {
+            if (failed) reject("API failed to fetch bugs");
+            else resolve(["UI glitch", "API timeout", "Login failure"]);
+        }, 1000);
+    });
+}
+getBugs()
+    .then(bugs => console.table(bugs))
+    .catch(err => console.log("Error:", err));
